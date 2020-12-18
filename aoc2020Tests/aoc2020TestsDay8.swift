f5b1c3e8af359f1de14b1ce74d5a3860f0e42850
Andrew McKnight <amcknight2718@gmail.com>
2020-12-18 02:28:06 -0900

    add day 8 solution

--- aoc2020Tests/aoc2020TestsDay8.swift
+++ aoc2020Tests/aoc2020TestsDay8.swift
@@ -10,18 +10,18 @@ import XCTest
 
 class aoc2020Day8Tests: XCTestCase {
     func testPart1Sample() {
-        XCTAssertEqual(day8Part1(day8SampleInput), -1)
+        XCTAssertEqual(day8Part1(day8SampleInput), 5)
     }
 
     func testPart1() {
-        XCTAssertEqual(day8Part1(day8Input), -1)
+        XCTAssertEqual(day8Part1(day8Input), 1087)
     }
 
     func testPart2Sample() {
-        XCTAssertEqual(day8Part2(day8SampleInput), -1)
+        XCTAssertEqual(day8Part2(day8SampleInput)!, 8)
     }
 
     func testPart2() {
-        XCTAssertEqual(day8Part2(day8Input), -1)
+        XCTAssertEqual(day8Part2(day8Input)!, 780)
     }
 }
