7d242e6c8d891916e0747e0dd9bb0800a49a120d
Andrew McKnight <amcknight2718@gmail.com>
2020-12-18 01:52:55 -0900

    add day 4 solution

--- aoc2020/Day4/Day4Part1.swift
+++ aoc2020/Day4/Day4Part1.swift
@@ -8,6 +8,39 @@
 import aocHelpers
 import Foundation
 
+extension String {
+    var integerValue: Int {
+        return (self as NSString).integerValue
+    }
+
+    func matches(_ regex: String) throws -> Bool {
+        let regex = try NSRegularExpression(pattern: regex, options: [])
+        let range = NSRange(location: 0, length: self.count)
+        let match = regex.firstMatch(in: self, options: [], range: range)
+        return match != nil
+    }
+
+    func enumerateMatches(with regex: String, block: ((NSTextCheckingResult?) -> Void)) throws {
+        let regex = try NSRegularExpression(pattern: regex, options: [])
+        let range = NSRange(location: 0, length: self.count)
+        regex.enumerateMatches(in: self, options: [], range: range) { (result, flags, stop) in
+            block(result)
+        }
+    }
+}
+
 public func day4Part1(_ input: String) -> Int {
-    return -1
+    return input.replacingOccurrences(of: "\n\n", with: ";")
+        .split(separator: ";")
+        .filter({$0.count > 0})
+        .map({String($0)})
+        .reduce(0, { (partial, next) -> Int in
+            let fields = next.replacingOccurrences(of: "\n", with: " ")
+                .split(separator: " ")
+            let hasCID = fields.contains { (string) -> Bool in
+                string.contains("cid")
+            }
+            let valid = (hasCID && fields.count == 8) || (!hasCID && fields.count == 7)
+            return partial + (valid ? 1 : 0)
+        })
 }
--- aoc2020Tests/aoc2020TestsDay4.swift
+++ aoc2020Tests/aoc2020TestsDay4.swift
@@ -10,18 +10,26 @@ import XCTest
 
 class aoc2020Day4Tests: XCTestCase {
     func testPart1Sample() {
-        XCTAssertEqual(day4Part1(day4SampleInput), -1)
+        XCTAssertEqual(day4Part1(day4SampleInput), 2)
     }
 
     func testPart1() {
-        XCTAssertEqual(day4Part1(day4Input), -1)
+        XCTAssertEqual(day4Part1(day4Input), 256)
     }
 
     func testPart2Sample() {
-        XCTAssertEqual(day4Part2(day4SampleInput), -1)
+        XCTAssertEqual(day4Part2(day4SampleInput), 2)
+    }
+
+    func testPart2Invalid() {
+        XCTAssertEqual(day4Part2(day4InvalidInput), 0)
+    }
+
+    func testPart2Valid() {
+        XCTAssertEqual(day4Part2(day4ValidInput), 4)
     }
 
     func testPart2() {
-        XCTAssertEqual(day4Part2(day4Input), -1)
+        XCTAssertEqual(day4Part2(day4Input), 198)
     }
 }
