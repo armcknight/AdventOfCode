a2136b69441466c134cba1b4664c426f56aad25f
Andrew McKnight <amcknight2718@gmail.com>
2020-12-27 20:53:22 -0900

    WIP day 21 part 1

--- aoc2020/Day21/Day21Part1.swift
+++ aoc2020/Day21/Day21Part1.swift
@@ -8,6 +8,23 @@
 import aocHelpers
 import Foundation
 
+struct Food {
+    var ingredients: [String]
+    var allergens: [String]
+}
+
 public func day21Part1(_ input: String) -> Int {
+    let foods = input.lines.map({ line -> Food in
+        var allergens = [String]()
+        var ingredients = [String]()
+        try! String(line).enumerateMatches(with: #"(.*) \(contains (.*)\)"#) { (match) in
+            ingredients = match[1, line].split(separator: " ").map { String($0) }
+            allergens = match[2, line].split(separator: ",").map { $0.trimmingCharacters(in: .whitespaces)}
+        }
+        return Food(ingredients: ingredients, allergens: allergens)
+    })
+
+
+
     return -1
 }
