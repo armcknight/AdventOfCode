35cbdfa8c7d65943860561f2a14602c77ba00049
Andrew McKnight <amcknight2718@gmail.com>
2021-12-03 19:54:15 -0900

    remove the unused zeros count

--- aoc2021/Day03/Day03Part1.swift
+++ aoc2021/Day03/Day03Part1.swift
@@ -9,8 +9,8 @@ import aocHelpers
 import FastMath
 import Foundation
 
-func counts(lines: [String]) -> (zeros: [Int], ones: [Int]) {
-    let ones = lines.reduce(into: Array<Int>(repeating: 0, count: lines.first!.count)) { (result, next) in
+func counts(lines: [String]) -> [Int] {
+    lines.reduce(into: Array<Int>(repeating: 0, count: lines.first!.count)) { (result, next) in
         var i = 0
         Array(next).forEach {
             if $0 == "1" {
@@ -19,22 +19,12 @@ func counts(lines: [String]) -> (zeros: [Int], ones: [Int]) {
             i += 1
         }
     }
-    let zeros = lines.reduce(into: Array<Int>(repeating: 0, count: lines.first!.count)) { (result, next) in
-        var i = 0
-        Array(next).forEach {
-            if $0 == "0" {
-                result[i] += 1
-            }
-            i += 1
-        }
-    }
-    return (zeros, ones)
 }
 
 public func day03Part1(_ input: String) -> Int {
     let lines = input.lines
     var i = 0
-    let gamma = counts(lines: lines).ones.reversed().reduce(into: 0) { (result, next) in
+    let gamma = counts(lines: lines).reversed().reduce(into: 0) { (result, next) in
         if next > lines.count / 2 {
             result += 2 ** i
         }
