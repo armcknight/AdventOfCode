786860bdd68fff1930470f144037775f72df881d
Andrew McKnight <amcknight2718@gmail.com>
2021-12-14 19:59:28 -0900

    day 10 part 1

--- aoc2021/Day10/Day10Part1.swift
+++ aoc2021/Day10/Day10Part1.swift
@@ -8,8 +8,68 @@
 import aocHelpers
 import Foundation
 
+enum Delimiter: String {
+    case openParen = "("
+    case closeParen = ")"
+    case openSquare = "["
+    case closeSquare = "]"
+    case openCurly = "{"
+    case closeCurly = "}"
+    case openAngle = "<"
+    case closeAngle = ">"
+    var score: Int {
+        switch self {
+        case .closeParen: return 3
+        case .closeSquare: return 57
+        case .closeCurly: return 1197
+        case .closeAngle: return 25137
+        default: return 0
+        }
+    }
+    var isOpener: Bool {
+        switch self {
+        case .openParen, .openSquare, .openCurly, .openAngle: return true
+        default: return false
+        }
+    }
+    func closes(opener: Delimiter) -> Bool {
+        switch self {
+        case .closeParen: return opener == .openParen
+        case .closeSquare: return opener == .openSquare
+        case .closeCurly: return opener == .openCurly
+        case .closeAngle: return opener == .openAngle
+        default: return false
+        }
+    }
+}
+
+extension String {
+    var firstIllegalToken: Delimiter? {
+        var stack = [Delimiter]()
+        for c in self {
+            let delim = Delimiter(rawValue: String(c))!
+            if delim.isOpener {
+                stack.append(delim)
+            } else {
+                if delim.closes(opener: stack.last!) {
+                    let _ = stack.popLast()
+                } else {
+                    return delim
+                }
+            }
+        }
+        return nil
+    }
+
+    var score: Int {
+        firstIllegalToken?.score ?? 0
+    }
+}
+
 public extension Day10 {
     var part1: Int {
-        return -1
+        rawValue.lines.reduce(0) { result, next in
+            result + next.score
+        }
     }
 }
--- aoc2021/Day10/Day10Tests.swift
+++ aoc2021/Day10/Day10Tests.swift
@@ -10,11 +10,11 @@ import XCTest
 
 class Day10Tests: XCTestCase {
     func testPart1Sample() {
-        XCTAssertEqual(Day10.sample.part1, -1)
+        XCTAssertEqual(Day10.sample.part1, 26397)
     }
 
     func testPart1() {
-        XCTAssertEqual(Day10.input.part1, -1)
+        XCTAssertEqual(Day10.input.part1, 390993)
     }
 
     func testPart2Sample() {
