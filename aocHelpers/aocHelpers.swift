bbe8a92b37c49af92df55a7955d9ccfcc1049cab
Andrew McKnight <andrew.mcknight@sentry.io>
2022-12-10 23:28:19 -0900

    move neighbor and sightline funtions to more generic extensions

--- aocHelpers/aocHelpers.swift
+++ aocHelpers/aocHelpers.swift
@@ -100,61 +100,8 @@ public extension String {
     }
 }
 
+// MARK: 2D Arrays of Integers
 public extension Array where Element == Array<Int> {
-    func sightLineRanges(row: Int, col: Int) -> (rowLeft: [Int], rowRight: [Int], colUp: [Int], colDown: [Int]) {
-        var colUpElements = [Int]()
-        for i in 0 ..< row {
-            colUpElements.append(self[i][col])
-        }
-
-        var colDownElements = [Int]()
-        for i in row + 1 ..< count {
-            colDownElements.append(self[i][col])
-        }
-
-        var rowLeftElements = [Int]()
-        let row = self[row]
-        for i in 0 ..< col {
-            rowLeftElements.append(row[i])
-        }
-
-        var rowRightElements = [Int]()
-        for i in col + 1 ..< row.count {
-            rowRightElements.append(row[i])
-        }
-
-        return (rowLeftElements, rowRightElements, colUpElements, colDownElements)
-    }
-
-    /// Return the 4 values neighboring the value of this cell. Cells on edges of the grid have `nil` values for invalid neighbor locations.
-    func neighbors4(row: Int, col: Int) -> (up: Int?, right: Int?, down: Int?, left: Int?) {
-        var left: Int? = nil
-        var up: Int? = nil
-        var down: Int? = nil
-        var right: Int? = nil
-
-        if col > 0 {
-            left = self[row][col - 1]
-        }
-        if row > 0 {
-            up = self[row - 1][col]
-        }
-        if col < self[row].count - 1 {
-            right = self[row][col + 1]
-        }
-        if row < self.count - 1 {
-            down = self[row + 1][col]
-        }
-
-        return (up: up, right: right, down: down, left: left)
-    }
-
-    /// Return the neighbors from `neighbors4(row:int:)` in an array in clockwise order starting from the value in the location directly above the specified location.
-    func neighbors4Array(row: Int, col: Int) -> [Int] {
-        let neighbors = neighbors4(row: row, col: col)
-        return [neighbors.up, neighbors.right, neighbors.down, neighbors.left].compactMap { return $0 }
-    }
-
     func neighbors4Coordinates(row: Int, col: Int) -> [CGPoint] {
         var coords = [CGPoint]()
         if col > 0 {
@@ -204,6 +151,7 @@ public extension Array where Element == Array<Int> {
     }
 }
 
+// MARK: 2D Collections of Strings
 public extension Collection where Element == Array<String> {
     func printGrid() {
         let separator = String(repeating: "-", count: first!.count * 2 - 1)
@@ -214,8 +162,33 @@ public extension Collection where Element == Array<String> {
     }
 }
 
-// MARK: Generic 2D Collection Functions
+// MARK: Generic 2D Collections
 public extension Collection where Iterator.Element: Collection, Self.Index == Int, Iterator.Element.Index == Int {
+    func sightLineRanges<T>(row: Int, col: Int) -> (rowLeft: [T], rowRight: [T], colUp: [T], colDown: [T]) {
+        var colUpElements = [T]()
+        for i in 0 ..< row {
+            colUpElements.append(self[i][col] as! T)
+        }
+
+        var colDownElements = [T]()
+        for i in row + 1 ..< count {
+            colDownElements.append(self[i][col] as! T)
+        }
+
+        var rowLeftElements = [T]()
+        let row = self[row]
+        for i in 0 ..< col {
+            rowLeftElements.append(row[i] as! T)
+        }
+
+        var rowRightElements = [T]()
+        for i in col + 1 ..< row.count {
+            rowRightElements.append(row[i] as! T)
+        }
+
+        return (rowLeftElements, rowRightElements, colUpElements, colDownElements)
+    }
+
     func enumerate<T>(rowStartOffset: Int = 0, rowEndOffset: Int = 0, colStartOffset: Int = 0, colEndOffset: Int = 0, _ block: (_ row: Int, _ col: Int, _ element: T) -> Void) {
         let rowEnd = count - rowEndOffset
         let colEnd = self[0].count - colEndOffset
@@ -229,57 +202,89 @@ public extension Collection where Iterator.Element: Collection, Self.Index == In
     func enumerate<T>(allOffsets: Int = 0, _ block: (_ row: Int, _ col: Int, _ element: T) -> Void) {
         enumerate(rowStartOffset: allOffsets, rowEndOffset: allOffsets, colStartOffset: allOffsets, colEndOffset: allOffsets, block)
     }
-}
 
-public extension Array where Element == Array<Any> {
-    /// Return the 8 values neighboring the value of this cell. Cells on edges of the grid have `nil` values for invalid neighbor locations.
-    func neighbors8(row: Int, col: Int) -> (up: Any?, upRight: Any?, right: Any?, rightDown: Any?, down: Any?, downLeft: Any?, left: Any?, leftUp: Any?) {
-        var left: Any? = nil
-        var upRight: Any? = nil
-        var up: Any? = nil
-        var rightDown: Any? = nil
-        var down: Any? = nil
-        var downLeft: Any? = nil
-        var right: Any? = nil
-        var leftUp: Any? = nil
+    typealias Neighbor4<T> = (up: T?, right: T?, down: T?, left: T?)
+
+    /// Return the 4 values neighboring the value of this cell. Cells on edges of the grid have `nil` values for invalid neighbor locations.
+    func neighbors4<T>(row: Int, col: Int) -> Neighbor4<T> {
+        var left: T? = nil
+        var up: T? = nil
+        var down: T? = nil
+        var right: T? = nil
 
         if col > 0 {
-            left = self[row][col - 1]
+            left = self[row][col - 1] as! T?
         }
         if row > 0 {
-            up = self[row - 1][col]
+            up = self[row - 1][col] as! T?
         }
         if col < self[row].count - 1 {
-            right = self[row][col + 1]
+            right = self[row][col + 1] as! T?
         }
         if row < self.count - 1 {
-            down = self[row + 1][col]
+            down = self[row + 1][col] as! T?
+        }
+
+        return (up: up, right: right, down: down, left: left)
+    }
+
+    /// Return the neighbors from `neighbors4(row:int:)` in an array in clockwise order starting from the value in the location directly above the specified location.
+    func neighbors4Array<T>(row: Int, col: Int) -> [T] {
+        let neighbors: Neighbor4<T> = neighbors4(row: row, col: col)
+        return [neighbors.up, neighbors.right, neighbors.down, neighbors.left].compactMap { return $0 }
+    }
+
+    typealias Neighbor8<T> = (up: T?, upRight: T?, right: T?, rightDown: T?, down: T?, downLeft: T?, left: T?, leftUp: T?)
+
+    /// Return the 8 values neighboring the value of this cell. Cells on edges of the grid have `nil` values for invalid neighbor locations.
+    func neighbors8<T>(row: Int, col: Int) -> Neighbor8<T> {
+        var left: T? = nil
+        var upRight: T? = nil
+        var up: T? = nil
+        var rightDown: T? = nil
+        var down: T? = nil
+        var downLeft: T? = nil
+        var right: T? = nil
+        var leftUp: T? = nil
+
+        if col > 0 {
+            left = self[row][col - 1] as! T?
+        }
+        if row > 0 {
+            up = self[row - 1][col] as! T?
+        }
+        if col < self[row].count - 1 {
+            right = self[row][col + 1] as! T?
+        }
+        if row < self.count - 1 {
+            down = self[row + 1][col] as! T?
         }
 
         if col > 0 && row > 0 {
-            leftUp = self[row - 1][col - 1]
+            leftUp = self[row - 1][col - 1] as! T?
         }
         if col < self[row].count - 1 && row > 0 {
-            upRight = self[row - 1][col + 1]
+            upRight = self[row - 1][col + 1] as! T?
         }
 
         if col > 0 && row < count - 1 {
-            downLeft = self[row + 1][col - 1]
+            downLeft = self[row + 1][col - 1] as! T?
         }
         if col < self[row].count - 1 && row < count - 1 {
-            rightDown = self[row + 1][col + 1]
+            rightDown = self[row + 1][col + 1] as! T?
         }
 
         return (up: up, upRight: upRight, right: right, rightDown: rightDown, down: down, downLeft: downLeft, left: left, leftUp: leftUp)
     }
 
     /// Return the neighbors from `neighbors8(row:int:)` in an array in clockwise order starting from the value in the location directly above the specified location.
-    func neighbors8Array(row: Int, col: Int) -> Element {
-        let neighbors = neighbors8(row: row, col: col)
+    func neighbors8Array<T>(row: Int, col: Int) -> [T] {
+        let neighbors: Neighbor8<T> = neighbors8(row: row, col: col)
         return [neighbors.up, neighbors.upRight, neighbors.right, neighbors.rightDown, neighbors.down, neighbors.downLeft, neighbors.left, neighbors.leftUp].compactMap { return $0 }
     }
 }
 
+// MARK: Arrays of Strings
 public extension Array<String> {
     /**
      * Given columnar input, return an array of columns, each of which is represented as an array of its elements.
