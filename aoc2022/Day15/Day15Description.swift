76fc42ba99124fe1871254dbf10b296e2b95d406
Andrew McKnight <andrew.mcknight@sentry.io>
2022-12-16 00:40:24 -0900

    get new problem descriptions

--- aoc2022/Day15/Day15Description.swift
+++ aoc2022/Day15/Day15Description.swift
@@ -6,14 +6,196 @@
 //
 
 /**
-{{ description }}
+## --- Day 15: Beacon Exclusion Zone ---
+
+You feel the ground rumble again as the distress signal leads you to a
+large network of subterranean tunnels. You don't have time to search
+them all, but you don't need to: your pack contains a set of deployable
+*sensors* that you imagine were originally built to locate lost Elves.
+
+The sensors aren't very powerful, but that's okay; your handheld device
+indicates that you're close enough to the source of the distress signal
+to use them. You pull the emergency sensor system out of your pack, hit
+the big button on top, and the sensors zoom off down the tunnels.
+
+Once a sensor finds a spot it thinks will give it a good reading, it
+attaches itself to a hard surface and begins monitoring for the nearest
+signal source *beacon*. Sensors and beacons always exist at integer
+coordinates. Each sensor knows its own position and can *determine the
+position of a beacon precisely*; however, sensors can only lock on to
+the one beacon *closest to the sensor* as measured by the
+<a href="https://en.wikipedia.org/wiki/Taxicab_geometry"
+target="_blank">Manhattan distance</a>. (There is never a tie where two
+beacons are the same distance to a sensor.)
+
+It doesn't take long for the sensors to report back their positions and
+closest beacons (your puzzle input). For example:
+
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+
+So, consider the sensor at `2,18`; the closest beacon to it is at
+`-2,15`. For the sensor at `9,16`, the closest beacon to it is at
+`10,16`.
+
+Drawing sensors as `S` and beacons as `B`, the above arrangement of
+sensors and beacons looks like this:
+
+1    1    2    2
+0    5    0    5    0    5
+0 ....S.......................
+1 ......................S.....
+2 ...............S............
+3 ................SB..........
+4 ............................
+5 ............................
+6 ............................
+7 ..........S.......S.........
+8 ............................
+9 ............................
+10 ....B.......................
+11 ..S.........................
+12 ............................
+13 ............................
+14 ..............S.......S.....
+15 B...........................
+16 ...........SB...............
+17 ................S..........B
+18 ....S.......................
+19 ............................
+20 ............S......S........
+21 ............................
+22 .......................B....
+
+This isn't necessarily a comprehensive map of all beacons in the area,
+though. Because each sensor only identifies its closest beacon, if a
+sensor detects a beacon, you know there are no other beacons that close
+or closer to that sensor. There could still be beacons that just happen
+to not be the closest beacon to any sensor. Consider the sensor at
+`8,7`:
+
+1    1    2    2
+0    5    0    5    0    5
+-2 ..........#.................
+-1 .........###................
+0 ....S...#####...............
+1 .......#######........S.....
+2 ......#########S............
+3 .....###########SB..........
+4 ....#############...........
+5 ...###############..........
+6 ..#################.........
+7 .#########S#######S#........
+8 ..#################.........
+9 ...###############..........
+10 ....B############...........
+11 ..S..###########............
+12 ......#########.............
+13 .......#######..............
+14 ........#####.S.......S.....
+15 B........###................
+16 ..........#SB...............
+17 ................S..........B
+18 ....S.......................
+19 ............................
+20 ............S......S........
+21 ............................
+22 .......................B....
+
+This sensor's closest beacon is at `2,10`, and so you know there are no
+beacons that close or closer (in any positions marked `#`).
+
+None of the detected beacons seem to be producing the distress signal,
+so you'll need to <span
+title="&quot;When you have eliminated all which is impossible, then whatever remains, however improbable, must be where the missing beacon is.&quot; - Sherlock Holmes">work
+out</span> where the distress beacon is by working out where it *isn't*.
+For now, keep things simple by counting the positions where a beacon
+cannot possibly be along just a single row.
+
+So, suppose you have an arrangement of beacons and sensors like in the
+example above and, just in the row where `y=10`, you'd like to count the
+number of positions a beacon cannot possibly exist. The coverage from
+all sensors near that row looks like this:
+
+1    1    2    2
+0    5    0    5    0    5
+9 ...#########################...
+10 ..####B######################..
+11 .###S#############.###########.
+
+In this example, in the row where `y=10`, there are *`26`* positions
+where a beacon cannot be present.
+
+Consult the report from the sensors you just deployed. *In the row where
+`y=2000000`, how many positions cannot contain a beacon?*
+
 */
 public enum Day15: String {
-    case sample = """
-{{ sample-input }}
+case sample = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
 """
 
-    case input = """
-{{ input }}
+case input = """
+Sensor at x=489739, y=1144461: closest beacon is at x=-46516, y=554951
+Sensor at x=2543342, y=3938: closest beacon is at x=2646619, y=229757
+Sensor at x=3182359, y=3999986: closest beacon is at x=3142235, y=3956791
+Sensor at x=3828004, y=1282262: closest beacon is at x=3199543, y=2310713
+Sensor at x=871967, y=3962966: closest beacon is at x=-323662, y=4519876
+Sensor at x=1323641, y=2986163: closest beacon is at x=2428372, y=3303736
+Sensor at x=2911492, y=2576579: closest beacon is at x=3022758, y=2461675
+Sensor at x=3030965, y=2469848: closest beacon is at x=3022758, y=2461675
+Sensor at x=3299037, y=3402462: closest beacon is at x=3142235, y=3956791
+Sensor at x=1975203, y=1672969: closest beacon is at x=1785046, y=2000000
+Sensor at x=3048950, y=2452864: closest beacon is at x=3022758, y=2461675
+Sensor at x=336773, y=2518242: closest beacon is at x=1785046, y=2000000
+Sensor at x=1513936, y=574443: closest beacon is at x=2646619, y=229757
+Sensor at x=3222440, y=2801189: closest beacon is at x=3199543, y=2310713
+Sensor at x=2838327, y=2122421: closest beacon is at x=2630338, y=2304286
+Sensor at x=2291940, y=2502068: closest beacon is at x=2630338, y=2304286
+Sensor at x=2743173, y=3608337: closest beacon is at x=2428372, y=3303736
+Sensor at x=3031202, y=2452943: closest beacon is at x=3022758, y=2461675
+Sensor at x=3120226, y=3998439: closest beacon is at x=3142235, y=3956791
+Sensor at x=2234247, y=3996367: closest beacon is at x=2428372, y=3303736
+Sensor at x=593197, y=548: closest beacon is at x=-46516, y=554951
+Sensor at x=2612034, y=2832157: closest beacon is at x=2630338, y=2304286
+Sensor at x=3088807, y=3929947: closest beacon is at x=3142235, y=3956791
+Sensor at x=2022834, y=2212455: closest beacon is at x=1785046, y=2000000
+Sensor at x=3129783, y=3975610: closest beacon is at x=3142235, y=3956791
+Sensor at x=3150025, y=2333166: closest beacon is at x=3199543, y=2310713
+Sensor at x=3118715, y=2376161: closest beacon is at x=3199543, y=2310713
+Sensor at x=3951193, y=3181929: closest beacon is at x=4344952, y=3106256
+Sensor at x=2807831, y=2401551: closest beacon is at x=2630338, y=2304286
+Sensor at x=3683864, y=2906786: closest beacon is at x=4344952, y=3106256
+Sensor at x=2723234, y=3206978: closest beacon is at x=2428372, y=3303736
+Sensor at x=3047123, y=3891244: closest beacon is at x=3142235, y=3956791
+Sensor at x=3621967, y=3793314: closest beacon is at x=3142235, y=3956791
+Sensor at x=2384506, y=1814055: closest beacon is at x=2630338, y=2304286
+Sensor at x=83227, y=330275: closest beacon is at x=-46516, y=554951
+Sensor at x=3343176, y=75114: closest beacon is at x=2646619, y=229757
 """
 }
--- aoc2022/Day16/Day16Description.swift
+++ aoc2022/Day16/Day16Description.swift
@@ -6,14 +6,256 @@
 //
 
 /**
-{{ description }}
+## --- Day 16: Proboscidea Volcanium ---
+
+The sensors have led you to the origin of the distress signal: yet
+another handheld device, just like the one the Elves gave you. However,
+you don't see any Elves around; instead, the device is surrounded by
+elephants! They must have gotten lost in these tunnels, and one of the
+elephants apparently figured out how to turn on the distress signal.
+
+The ground rumbles again, much stronger this time. What kind of cave is
+this, exactly? You scan the cave with your handheld device; it reports
+mostly igneous rock, some ash, pockets of pressurized gas, magma... this
+isn't just a cave, it's a volcano!
+
+You need to get the elephants out of here, quickly. Your device
+estimates that you have *30 minutes* before the volcano erupts, so you
+don't have time to go back out the way you came in.
+
+You scan the cave for other options and discover a network of pipes and
+pressure-release *valves*. You aren't sure how such a system got into a
+volcano, but you don't have time to complain; your device produces a
+report (your puzzle input) of each valve's *flow rate* if it were opened
+(in pressure per minute) and the tunnels you could use to move between
+the valves.
+
+There's even a valve in the room you and the elephants are currently
+standing in labeled `AA`. You estimate it will take you one minute to
+open a single valve and one minute to follow any tunnel from one valve
+to another. What is the most pressure you could release?
+
+For example, suppose you had the following scan output:
+
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
+
+All of the valves begin *closed*. You start at valve `AA`, but it must
+be damaged or <span
+title="Wait, sir! The valve, sir! it appears to be... jammed!">jammed</span>
+or something: its flow rate is `0`, so there's no point in opening it.
+However, you could spend one minute moving to valve `BB` and another
+minute opening it; doing so would release pressure during the remaining
+*28 minutes* at a flow rate of `13`, a total eventual pressure release
+of `28 * 13 = `*`364`*. Then, you could spend your third minute moving
+to valve `CC` and your fourth minute opening it, providing an additional
+*26 minutes* of eventual pressure release at a flow rate of `2`, or
+*`52`* total pressure released by valve `CC`.
+
+Making your way through the tunnels like this, you could probably open
+many or all of the valves by the time 30 minutes have elapsed. However,
+you need to release as much pressure as possible, so you'll need to be
+methodical. Instead, consider this approach:
+
+== Minute 1 ==
+No valves are open.
+You move to valve DD.
+
+== Minute 2 ==
+No valves are open.
+You open valve DD.
+
+== Minute 3 ==
+Valve DD is open, releasing 20 pressure.
+You move to valve CC.
+
+== Minute 4 ==
+Valve DD is open, releasing 20 pressure.
+You move to valve BB.
+
+== Minute 5 ==
+Valve DD is open, releasing 20 pressure.
+You open valve BB.
+
+== Minute 6 ==
+Valves BB and DD are open, releasing 33 pressure.
+You move to valve AA.
+
+== Minute 7 ==
+Valves BB and DD are open, releasing 33 pressure.
+You move to valve II.
+
+== Minute 8 ==
+Valves BB and DD are open, releasing 33 pressure.
+You move to valve JJ.
+
+== Minute 9 ==
+Valves BB and DD are open, releasing 33 pressure.
+You open valve JJ.
+
+== Minute 10 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve II.
+
+== Minute 11 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve AA.
+
+== Minute 12 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve DD.
+
+== Minute 13 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve EE.
+
+== Minute 14 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve FF.
+
+== Minute 15 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve GG.
+
+== Minute 16 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You move to valve HH.
+
+== Minute 17 ==
+Valves BB, DD, and JJ are open, releasing 54 pressure.
+You open valve HH.
+
+== Minute 18 ==
+Valves BB, DD, HH, and JJ are open, releasing 76 pressure.
+You move to valve GG.
+
+== Minute 19 ==
+Valves BB, DD, HH, and JJ are open, releasing 76 pressure.
+You move to valve FF.
+
+== Minute 20 ==
+Valves BB, DD, HH, and JJ are open, releasing 76 pressure.
+You move to valve EE.
+
+== Minute 21 ==
+Valves BB, DD, HH, and JJ are open, releasing 76 pressure.
+You open valve EE.
+
+== Minute 22 ==
+Valves BB, DD, EE, HH, and JJ are open, releasing 79 pressure.
+You move to valve DD.
+
+== Minute 23 ==
+Valves BB, DD, EE, HH, and JJ are open, releasing 79 pressure.
+You move to valve CC.
+
+== Minute 24 ==
+Valves BB, DD, EE, HH, and JJ are open, releasing 79 pressure.
+You open valve CC.
+
+== Minute 25 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+== Minute 26 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+== Minute 27 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+== Minute 28 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+== Minute 29 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+== Minute 30 ==
+Valves BB, CC, DD, EE, HH, and JJ are open, releasing 81 pressure.
+
+This approach lets you release the most pressure possible in 30 minutes
+with this valve layout, *`1651`*.
+
+Work out the steps to release the most pressure in 30 minutes. *What is
+the most pressure you can release?*
+
 */
 public enum Day16: String {
-    case sample = """
-{{ sample-input }}
+case sample = """
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
 """
 
-    case input = """
-{{ input }}
+case input = """
+Valve JI has flow rate=21; tunnels lead to valves WI, XG
+Valve DM has flow rate=3; tunnels lead to valves JX, NG, AW, BY, PF
+Valve AZ has flow rate=0; tunnels lead to valves FJ, VC
+Valve YQ has flow rate=0; tunnels lead to valves TE, OP
+Valve WI has flow rate=0; tunnels lead to valves JI, VC
+Valve NE has flow rate=0; tunnels lead to valves ZK, AA
+Valve FM has flow rate=0; tunnels lead to valves LC, DU
+Valve QI has flow rate=0; tunnels lead to valves TE, JW
+Valve OY has flow rate=0; tunnels lead to valves XS, VF
+Valve XS has flow rate=18; tunnels lead to valves RR, OY, SV, NQ
+Valve NU has flow rate=0; tunnels lead to valves IZ, BD
+Valve JX has flow rate=0; tunnels lead to valves DM, ZK
+Valve WT has flow rate=23; tunnels lead to valves OV, QJ
+Valve KM has flow rate=0; tunnels lead to valves TE, OL
+Valve NG has flow rate=0; tunnels lead to valves II, DM
+Valve FJ has flow rate=0; tunnels lead to valves AZ, II
+Valve QR has flow rate=0; tunnels lead to valves ZK, KI
+Valve KI has flow rate=9; tunnels lead to valves ZZ, DI, TL, AJ, QR
+Valve ON has flow rate=0; tunnels lead to valves LC, QT
+Valve AW has flow rate=0; tunnels lead to valves DM, AA
+Valve HI has flow rate=0; tunnels lead to valves TE, VC
+Valve XG has flow rate=0; tunnels lead to valves II, JI
+Valve II has flow rate=19; tunnels lead to valves LF, NG, OL, FJ, XG
+Valve VC has flow rate=24; tunnels lead to valves WI, HI, AZ
+Valve VJ has flow rate=0; tunnels lead to valves UG, AA
+Valve IZ has flow rate=0; tunnels lead to valves VF, NU
+Valve EJ has flow rate=0; tunnels lead to valves ZK, LC
+Valve DU has flow rate=12; tunnels lead to valves TC, UG, FM
+Valve ZK has flow rate=10; tunnels lead to valves JX, EJ, JW, QR, NE
+Valve XF has flow rate=25; tunnels lead to valves OP, VT
+Valve LC has flow rate=4; tunnels lead to valves FM, EJ, ON, AJ, PF
+Valve SV has flow rate=0; tunnels lead to valves XS, IY
+Valve LF has flow rate=0; tunnels lead to valves II, OV
+Valve DI has flow rate=0; tunnels lead to valves KI, BY
+Valve OP has flow rate=0; tunnels lead to valves YQ, XF
+Valve NQ has flow rate=0; tunnels lead to valves TC, XS
+Valve QJ has flow rate=0; tunnels lead to valves VT, WT
+Valve IY has flow rate=22; tunnel leads to valve SV
+Valve AJ has flow rate=0; tunnels lead to valves LC, KI
+Valve TE has flow rate=11; tunnels lead to valves QI, HI, KM, YQ
+Valve ZZ has flow rate=0; tunnels lead to valves KI, AA
+Valve VT has flow rate=0; tunnels lead to valves XF, QJ
+Valve OL has flow rate=0; tunnels lead to valves KM, II
+Valve TC has flow rate=0; tunnels lead to valves NQ, DU
+Valve TL has flow rate=0; tunnels lead to valves VF, KI
+Valve QT has flow rate=0; tunnels lead to valves AA, ON
+Valve BY has flow rate=0; tunnels lead to valves DM, DI
+Valve OV has flow rate=0; tunnels lead to valves LF, WT
+Valve VN has flow rate=0; tunnels lead to valves RR, BD
+Valve VF has flow rate=13; tunnels lead to valves OY, IZ, TL
+Valve BD has flow rate=17; tunnels lead to valves NU, VN
+Valve UG has flow rate=0; tunnels lead to valves VJ, DU
+Valve PF has flow rate=0; tunnels lead to valves LC, DM
+Valve RR has flow rate=0; tunnels lead to valves XS, VN
+Valve AA has flow rate=0; tunnels lead to valves QT, ZZ, AW, VJ, NE
+Valve JW has flow rate=0; tunnels lead to valves ZK, QI
 """
 }
