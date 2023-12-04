c6eeab7419555698585628384a743438832c6422
Andrew McKnight <git@mcknight.rocks>
2023-12-03 15:32:44 -0900

    fix accidentally commented code

--- update/main.swift
+++ update/main.swift
@@ -8,18 +8,18 @@
 import Foundation
 import Then
 
-//func getYear(year: Int) {
-//    createSourceFiles(for: year)
-//    injectMissingTestTasks(for: year)
-//}
-//
-//if let year = ProcessInfo.processInfo.environment["year"], let yearValue = Int(year) {
-//    getYear(year: yearValue)
-//} else {
-//    for year in 2015 ... AoC.Date.currentYear {
-//        getYear(year: year)
-//    }
-//}
+func getYear(year: Int) {
+    createSourceFiles(for: year)
+    injectMissingTestTasks(for: year)
+}
+
+if let year = ProcessInfo.processInfo.environment["year"], let yearValue = Int(year) {
+    getYear(year: yearValue)
+} else {
+    for year in 2015 ... AoC.Date.currentYear {
+        getYear(year: year)
+    }
+}
 
 generateXcodeGenSpec()
 generateXcodeProject()
